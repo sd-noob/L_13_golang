e9644edbe71fdc939e1e19bc92bc6d9fa8a0edca
sd-noob <sdhriday77@gmail.com>
2021-02-20 18:46:54 +0600

    5th example of function

--- function.go
+++ function.go
@@ -1,16 +1,18 @@
-//Example -4:(Name return value)
+//Example -5:
 
 package main
 
 import "fmt"
 
-func add(x , y int) (r int){
-r = x+y
-return r
+func rectangle(l int, b int) (area int, parameter int) {
+	parameter = 2 * (l + b)
+	area = l * b
+	return // Return statement without specify variable name
 }
 
-func main(){
-x := add( 10, 30)
-fmt.Println(x)
-
+func main() {
+	var a, p int
+	a, p = rectangle(20, 30)
+	fmt.Println("Area:", a)
+	fmt.Println("Parameter:", p)
 }
